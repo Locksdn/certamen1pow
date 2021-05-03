3682f6673512c0377203050d7c81e014595e8f06
Sebastian Gaete <locksdanny@outlook.com>
2021-05-03 16:45:35 -0400

    Funcionalidad de Tabla Agregada

--- js/app.js
+++ js/app.js
@@ -16,6 +16,39 @@ tinymce.init({
 
 const actualizarLista = ()=>{
     let tbody = document.getElementById('tabla-reos');
+    tbody.innerHTML = '';
+    for(let i=0;i<reos.length;++i){
+        let r = reos[i];
+        let tr = document.createElement('tr');
+        let tdNombre = document.createElement('td');
+        let tdDetalle = document.createElement('td');
+        let tdCiudad = document.createElement('td');
+        let tdGravedad = document.createElement('td');
+
+        tdNombre.innerText = r.nombre;
+        tdDetalle.innerHTML = r.detalle;
+        tdCiudad.innerText = ciudades[r.ciudad];
+
+        let gravedad = document.createElement('i');
+        if(r.cantidad < 4){
+            gravedad.classList.add('fas', 'fa-user-ninja');
+        } else if (r.cantidad < 7){
+            gravedad.classList.add('fas', 'fa-exclamation-circle');
+        } else if (r.cantidad < 16){
+            gravedad.classList.add('fas', 'fa-exclamation-triangle');
+        } else {
+            gravedad.classList.add('fas', 'fa-skull-crossbones');
+        }
+        tdGravedad.classList.add('text-center');
+        tdGravedad.appendChild(gravedad);
+
+        tr.appendChild(tdNombre);
+        tr.appendChild(tdDetalle);
+        tr.appendChild(tdCiudad);
+        tr.appendChild(tdGravedad);
+
+        tbody.appendChild(tr);
+    }
 }
 
 
@@ -41,11 +74,11 @@ document.querySelector('#agregar-reo').addEventListener('click', ()=>{
 
     let reo = {};
     reo.nombre = nombre + ' ' + apellido;
-    reo.cantidad = cantidad;
+    reo.cantidad = Number(cantidad);
     reo.detalle = detalle;
     reo.ciudad = ciudad;
     reos.push(reo);
-
+    actualizarLista();
     Swal.fire(
         'Reo Agregado',
         'Se ha agregado un reo a la tabla exitosamente.',
